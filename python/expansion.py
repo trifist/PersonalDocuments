b80101fde73431b724d00aa9df1ebd922df0b9e5
wpcheng <wpcheng@iflytek.com>
2017-11-14 10:49:06 +0800

    修改代码，增加输出到文件

--- python/expansion.py
+++ python/expansion.py
@@ -7,15 +7,16 @@ class Node:
 
 nodes = []
 
-def expand(node, string):
+def expand(node, string, fo):
     for d in node.data:
         if(node.nextNode):
-            expand(node.nextNode, string + d)
+            expand(node.nextNode, string + d, fo)
         else:
-            print(string + d + "|\n")
+            fo.write(string + d + "|\n")
+            print(string + d + "|")
 
 def main():
-    rawData = input("Please input data, 'N' for exit:\n")
+    rawData = input("Please input data, press 'N' to exit:\n")
     while(rawData != "N"):
         node = Node()
         node.data = rawData.split("|")
@@ -25,7 +26,10 @@ def main():
     for i in range(len(nodes)-1):
         nodes[i].nextNode = nodes[i+1]
 
-    expand(nodes[0], "")
+    fo = open("./result.txt", "w", encoding="gbk")
+    expand(nodes[0], "", fo)
+    fo.close()
+    input("Press Enter to exit")
     return 0
 
 if(__name__=="__main__"):
