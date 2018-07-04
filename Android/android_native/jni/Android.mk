c905d09310e6de47f36b2bc38a9449ab34ae3154
wpcheng <wpcheng@iflytek.com>
2018-07-04 17:44:28 +0800

    修改android的jni使用部分

--- Android/android_native/jni/Android.mk
+++ Android/android_native/jni/Android.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_MODULE    := hello
-LOCAL_SRC_FILES := hello.cpp
+LOCAL_MODULE    := hello	（模块名称，最终会命名成libXXX.so）
+LOCAL_SRC_FILES := hello.cpp （涉及到的源文件，多个以空格分开）
 include $(BUILD_SHARED_LIBRARY)
--- Android/android_native/jni/Application.mk
+++ Android/android_native/jni/Application.mk
@@ -1,5 +1,5 @@
-APP_BUILD_SCRIPT := C:\Users\admin\Desktop\jni\jni\Android.mk
+APP_BUILD_SCRIPT := C:\Users\admin\Desktop\jni\jni\Android.mk （改成Android.mk所在的路径）
 APP_PLATFORM := android-19
-APP_ABI := all
+APP_ABI := all  （all表示所有，可选armeabi-v7a, arm64-v8a, x86, x86_64, mips, mips64。多个以空格分开）
 APP_STL := system
-APP_OPTIM := debug
+APP_OPTIM := debug （debug或release）
