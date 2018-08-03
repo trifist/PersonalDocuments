863d0933a05761171a37b0058144dacfa0a23b11
wpcheng <wpcheng@iflytek.com>
2018-08-03 14:10:30 +0800

    增加ndk调用stl库的支持

--- Android/android_native/jni/Application.mk
+++ Android/android_native/jni/Application.mk
@@ -2,4 +2,6 @@ APP_BUILD_SCRIPT := C:\Users\admin\Desktop\jni\jni\Android.mk （改成Android.m
 APP_PLATFORM := android-19
 APP_ABI := all  （all表示所有，可选armeabi-v7a, arm64-v8a, x86, x86_64, mips, mips64。多个以空格分开）
 APP_STL := system
+APP_STL := stlport_shared  （为了调用stl库而进行的引用）
+APP_STL := stlport_static  （为了调用stl库而进行的引用）
 APP_OPTIM := debug （debug或release）
